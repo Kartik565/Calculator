d8029b4a79e92af1dd7c5eb19617b6648dea0563
Kartik565 <kartik.jain0809@gmail.com>
2024-11-29 01:57:11 +0530

    calculator

---
+++ app.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll('button');
+let input = document.querySelector('input');
+
+for(let button of buttons){
+      button.addEventListener('click' , function(e){
+        let btnText = e.target.innerText;
+        if(btnText === 'C'){
+            input.value = '';
+        }
+        else if (btnText === "="){
+            try{
+                input.value = eval(input.value);
+            }
+            catch{
+                input.value = 'Gian hai aap'
+            }
+        }
+        else {
+            input.value += btnText; 
+        }
+      })
+}
